b693f6923d83ae948935f0cc63366bfb0ae1add9
Paco Vela <franciscojosevela@gmail.com>
2019-04-29 17:28:06 +0200

    version in composer

--- ModernTheme.php
+++ ModernTheme.php
@@ -29,7 +29,7 @@ class ModernTheme extends AbstractTheme implements HtmlThemeInterface
      */
     public function getName()
     {
-        return 'Modern_selectable';
+        return 'Modern';
     }
 
     /**
